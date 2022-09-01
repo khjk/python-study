d02053fe4048db6528bac79043785b703a959ec4
khjk <begywjd@naver.com>
2022-09-01 17:17:25 +0900

    스크랩핑

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from requests import get
+from bs4 import BeautifulSoup
 
 base_url = "https://weworkremotely.com/remote-jobs/search?term="
 search_term = "python"
@@ -8,4 +9,23 @@ response = get(f"{base_url}{search_term}")
 if response.status_code != 200:
     print("Can't request website")
 else:
-    print(response.text)
+    results = []
+    soup = BeautifulSoup(response.text, "html.parser")
+    jobs = soup.find_all('section', class_="jobs") #keyword argument: 순서 상관 없음
+    for job_section in jobs:
+        job_posts = job_section.find_all('li')
+        job_posts.pop(-1) #list에서 마지막을 제거
+        for post in job_posts:
+            anchors = post.find_all('a')
+            anchor = anchors[1]
+            link = anchor['href']
+            company, kind, region = anchor.find_all('span', class_="company")
+            title = anchor.find('span', class_='title')
+            job_data = {
+                'company' : company.string,
+                'region' : region.string,
+                'position' : title.string
+            }
+            results.append(job_data)
+    for result in results:
+        print(result)
