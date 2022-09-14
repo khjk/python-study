0df290c7ccc455b337cb152633a5d480fcf9e3c3
khjk <begywjd@naver.com>
2022-09-14 14:19:07 +0900

    selenium + beautifuSoup

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from selenium import webdriver
+from requests import get
+import time
+from bs4 import BeautifulSoup
+
 # extractors 폴더의 wwr파일에서 extract_wwr_jobs라는 함수를 import
 from extractors.wwr import extract_wwr_jobs
 
 browser = webdriver.Chrome()
 
 browser.get("https://www.indeed.com/jobs?q=python&limit=50")
+time.sleep(1)
 
-print(browser.page_source)
+response = browser.page_source
+soup = BeautifulSoup(response, "html.parser")
+job_list = soup.find("ul", class_="jobsearch-ResultsList")
+jobs = job_list.find_all('li', recursive=False)
+for job in jobs:
+    zone = job.find("div", class_="mosaic-zone")
+    if zone == None:
+        anchor = job.select("h2 a")
+        title = anchor['aria-label']
+        link = anchor['href']
+        print(title, link)
