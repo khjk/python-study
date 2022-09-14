c8d93bc28807fb124156a177f2c259350ac5a85b
khjk <begywjd@naver.com>
2022-09-14 14:19:21 +0900

    selenium+bs4

--- main.py
+++ main.py
@@ -15,10 +15,19 @@ response = browser.page_source
 soup = BeautifulSoup(response, "html.parser")
 job_list = soup.find("ul", class_="jobsearch-ResultsList")
 jobs = job_list.find_all('li', recursive=False)
+results = []
 for job in jobs:
     zone = job.find("div", class_="mosaic-zone")
     if zone == None:
-        anchor = job.select("h2 a")
+        anchor = job.select_one("h2 a")
         title = anchor['aria-label']
         link = anchor['href']
-        print(title, link)
+        company = job.find("span", class_="companyName")
+        location = job.find("div", class_="companyLocation")
+        job_data = {
+            'link': f"https://kr.indeed.com{link}",
+            'company': company.string,
+            'location': location.string,
+            'position': title.string
+        }
+        results.append(job_data)
